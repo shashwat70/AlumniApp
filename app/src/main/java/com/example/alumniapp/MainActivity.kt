570695814d7c3c62ee034c5ff3b91348bc46ea5f
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2020-06-12 16:08:29 +0530

    added menu and splash page

--- app/src/main/java/com/example/alumniapp/MainActivity.kt
+++ app/src/main/java/com/example/alumniapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.alumniapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item:MenuItem) : Boolean{
+        when(item.itemId){
+            R.id.search -> {
+                Toast.makeText(this@MainActivity, "search", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+                return true
+            }
+            else -> return false
+        }
     }
 }
